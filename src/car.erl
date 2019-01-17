83e55fef984e1fe90615713a061b79692cc5c3e6
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-01-18 00:32:02 +0100

    working on car signals

--- src/car.erl
+++ src/car.erl
@@ -15,16 +15,9 @@ init(Creator, Position, Direction, X, Y, GuiPid, CrossPid) ->
 
 car_lifecycle_loop(Position, Direction, X, Y, GuiPid, CrossPid) ->
 
-  io:format("coord of car: X = ~p, Y = ~p~n", [X, Y]),
-%%  io:format([CrossPid]),
-
   timer:sleep(300),
 
-%%  GuiPid ! {self(), X, Y, moved},
-
   CrossPid ! {self(), X, Y, moved},
-
-
 
 
   if
